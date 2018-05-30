3a8fcfbc8655abf78a9d5493b6cbfdb4baa344a6
HarwordLiu <harwordliu@gmail.com>
2018-05-30 18:12:49 +0800

    fix bug

--- scripts/xcodebuild_dev_config.sh
+++ scripts/xcodebuild_dev_config.sh
@@ -33,7 +33,7 @@ fi
 xcodebuild -$work_type ${work_path}/$target_name -scheme $project_name -configuration Debug -sdk iphoneos clean
 xcodebuild archive -$work_type ${work_path}/$target_name -scheme $project_name -configuration Debug -archivePath ${out_path}/$project_name.xcarchive
 
-xcodebuild -exportArchive -archivePath ${out_path}/XKSDeliver.xcarchive -exportPath ${out_path} -exportOptionsPlist ${sctipt_path}/xcodebuild_dev_config.plist
+xcodebuild -exportArchive -archivePath ${out_path}/$project_name.xcarchive -exportPath ${out_path} -exportOptionsPlist ${sctipt_path}/xcodebuild_dev_config.plist
 
 echo ${out_path}/$project_name.ipa
 
